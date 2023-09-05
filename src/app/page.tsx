92bfc97107e1c2ebaa7b78bddc6ab091a78d4743
nashdev255 <yutori1107@gmail.com>
2023-09-05 17:48:25 +0900

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,176 @@
 import Image from 'next/image'
+import haru_pic from './imgs/haru_free_400x400.jpg'
+import twitter_pic from './imgs/twitter_logo.png'
+import bili_pic from './imgs/bilibili_logo.png'
+import yt_pic from './imgs/youtube_logo.jpg'
+import qiita_pic from './imgs/qiita_logo.png'
+import hate_pic from './imgs/hatena_logo.png'
+import github_pic from './imgs/github_logo.png'
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    return (
+        <>
+            <header>
+                <div className="header glass">
+                    <div className="logstate"></div>
+                    <ul className="navigation">
+                        <li><a href="#about">ABOUT</a></li>
+                        <li><a href="#works">WORKS</a></li>
+                        <li className="last-child"><a href="javascript:alert('まだ作ってない(´・ω・｀)')">CONTACT</a></li>
+                    </ul>
+                </div>
+                <div className="headline">
+                    <div className="title">
+                        <h1>Nash's Portfolio</h1>
+                        <p>here is my developing contents</p>
+                    </div>
+                </div>
+            </header>
+            <div id="about" className="introduction">
+                <h1 className="who">Who are you?</h1>
+                <div className="appearance">
+                    <Image src={ haru_pic } alt="nash's icon" />
+                    <div className="explanation">
+                        <h1>なっしゅ/nash</h1>
+                        <p>
+                            鈴鹿高専(NITSC)の情報科r04期生<br />
+                            趣味 : 自作OS, 3DCG<br />
+                            特技 : ある？<br />
+                            好きな食べ物 : 柚子胡椒<br />
+                            好きな言語 : C, NASM
+                        </p>
+                    </div>
+                </div>
+                <ul className="timeline">
+                    <li>
+                        <p className="timeline-date">2021年9月</p>
+                        <div className="timeline-content">
+                            <h3>BlenderとHTML&CSSに触れる</h3>
+                            <p>
+                                これまでパソコンで何かをすることはなかったが
+                                友人の薦められてBlenderとHTML&CSSに触れる。
+                                これが高専への入学を大きく後押しする。
+                                受験期だったため触れ始めてから2カ月ほどで一旦やめることになる。
+                            </p>
+                        </div>
+                    </li>
+                    <li>
+                        <p className="timeline-date">2022年3月</p>
+                        <div className="timeline-content">
+                            <h3>受験から復帰、自作PCを開発する</h3>
+                            <p>
+                                Blenderの作品作りに復帰する。
+                                同時期に名古屋のパソコンショップ(特に佐古前装備さん)に通い、
+                                非実用的ではあるがパソコンを自作する。
+                            </p>
+                        </div>
+                    </li>
+                    <li>
+                        <p className="timeline-date">2022年4月</p>
+                        <div className="timeline-content">
+                            <h3>鈴鹿高専に入学する</h3>
+                            <p>
+                                入学当日、入学前にネットで知り合った人と交流する。
+                                それからゴールデンウィークまで毎日演習室でC++を勉強したり、
+                                Arduinoで遊んだり、COCET2600の単語帳を回したりする。
+                            </p>
+                        </div>
+                    </li>
+                    <li>
+                        <p className="timeline-date">2022年9月</p>
+                        <div className="timeline-content">
+                            <h3>初バイト、Tomisuke配列を使い始める</h3>
+                            <p>
+                                バイトで稼いでRaspberryPi Model3Bを買ってファイルサーバを
+                                構築したり、自作キーボードを作ったりする。この頃に、Tomisuke配列
+                                を使い始めてタイピング速度が500kpmに到達する。
+                            </p>
+                        </div>
+                    </li>
+                    <li>
+                        <p className="timeline-date">2022年10月</p>
+                        <div className="timeline-content">
+                            <h3>とにかくアニメを観る</h3>
+                            <p>
+                                テスト前にも関わらず、アニメ配信サービスを契約して
+                                次々とアニメをイッキ見する。
+                                特に気に入った作品は
+                                『魔女の旅々』『無職転生』
+                                『ぼっち・ざ・ろっく！』『魔法少女まどかマギカ』など。
+                            </p>
+                        </div>
+                    </li>
+                    <li id="last-child-bottom">
+                        <p className="timeline-date">2022年12月</p>
+                        <div className="timeline-content">
+                            <h3>自作OSを始める</h3>
+                            <p>
+                                書籍『OS自作入門』を読み進めながら自作OSを始める。
+                                nasmの記述やメモリ管理など、低レイヤの知識を蓄える。
+                            </p>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <div className="view-contents">
+                <h1 className="contents-title">SNS</h1>
+                <div className="container">
+                    <div className="content glass">
+                        <Image src={ twitter_pic } alt="twitter-logo" />
+                        <div className="inner">
+                            <a href="https://twitter.com/nash_4ed9" target="_blank">
+                                <h2>Twitter</h2>
+                                <p>@nash_4ed9</p>
+                            </a>
+                        </div>
+                    </div>
+                    <div className="content glass">
+                        <Image src={ yt_pic } alt="youtube-logo" />
+                        <div className="inner">
+                            <a href="https://www.youtube.com/channel/UCWnqwRNf5TyLCfvTPzvQnLA" target="_blank">
+                                <h2>Youtube</h2>
+                                <p>@_nash</p>
+                            </a>
+                        </div>
+                    </div>
+                    <div className="content glass">
+                        <Image src={ bili_pic } alt="bilibili-logo" />
+                        <div className="inner">
+                            <a href="https://account.bilibili.com/account/home?spm_id_from=333.999.0.0" target="_blank">
+                                <h2>Bilibili</h2>
+                                <p>@bili_nash(未投稿)</p>
+                            </a>
+                        </div>
+                    </div>
+                    <div className="content glass">
+                        <Image src={ qiita_pic } alt="qiita-logo" />
+                        <div className="inner">
+                            <a href="https://qiita.com/nash_4ed9" target="_blank">
+                                <h2>Qiita</h2>
+                                <p>@nash_4ed9</p>
+                            </a>
+                        </div>
+                    </div>
+                    <div className="content glass">
+                        <Image src={ hate_pic } alt="hatena-logo" />
+                        <div className="inner">
+                            <a href="https://blog.hatena.ne.jp/nash_4ed9/nash-4ed9.hatenadiary.com/" target="_blank">
+                                <h2>Hatena Blog</h2>
+                                <p>@nash_4ed9(閉鎖)</p>
+                            </a>
+                        </div>
+                    </div>
+                    <div className="content glass">
+                        <Image src={ github_pic } alt="github-logo" />
+                        <div className="inner">
+                            <a href="https://github.com/nashdev255" target="_blank">
+                                <h2>GitHub</h2>
+                                <p>@yutori10</p>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
